5835401646f0ecab1d028714f6cd7b0578deef38
Jakob <jakobjakobjakob2@gmail.com>
2025-01-25 20:50:11 +0100

    progress on UI, timeline

--- clipcutter/src/ui.cc
+++ clipcutter/src/ui.cc
@@ -1,7 +1,122 @@
 #include "pch.h"
 #include "app.h"
 
+int tracklistWidth = 100;
+
+
+int trackCount = 2;
+int track1Height = 30;
+
 void UI_DrawEditor(App* app) {
+
+	if (ImGui::BeginMainMenuBar()) {
+		if (ImGui::Button("Load File")) {
+
+		}
+		ImGui::EndMainMenuBar();
+	}
+
+	if (ImGui::Begin("DebugThingies")) {
+		ImGui::Text("playbacktime");
+		ImGui::Text("cutoffstart");
+		ImGui::Text("cutoffend");
+		ImGui::Text("scaling");
+		//local iPtr = ffi.new("int[1]", 0)
+		//iPtr[0] = track1Height
+		//imgui.SliderInt("trackHeight", iPtr, 10, 100, "%d")
+		//track1Height = iPtr[0]
+		//iPtr[0] = trackCount
+		//imgui.SliderInt("trackCount", iPtr, 0, 50, "%d")
+		//trackCount = iPtr[0]
+		//iPtr[0] = snappingPrecision
+		//imgui.SliderInt("snappingPrecision", iPtr, 1, 50, "%d") --disabled when 1
+		//snappingPrecision = iPtr[0]
+
+	}
+	ImGui::End();
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding,  ImVec2(0, 0));
+
+	if (ImGui::Begin("Timeline")) {
+
+		ImVec2 cursorTrackListBefore;
+		ImVec2 cursorTracklistAfter;
+		{ // Tracklist
+			ImGui::BeginGroup();
+			ImU32 tracklistColor = ImGui::GetColorU32(ImVec4(0.15, 0.15, 0.15, 1));
+			//ImVec2 tracklistSize = ImVec2(tracklistWidth, std::max(ImGui::GetContentRegionAvail().y, (trackCount + 3) * track1Height));
+			ImVec2 tracklistSize = ImVec2(tracklistWidth, fmax(ImGui::GetContentRegionAvail().y, (float)((trackCount + 3) * track1Height)));
+
+			cursorTrackListBefore = ImGui::GetCursorScreenPos();
+			ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+			ImGui::Dummy(tracklistSize);
+			ImGui::PopStyleVar();
+			ImVec2 r_min = ImGui::GetItemRectMin();
+			ImVec2 r_max= ImGui::GetItemRectMax();
+			cursorTracklistAfter = ImGui::GetCursorScreenPos();
+
+			ImDrawList* timelineDrawlist = ImGui::GetWindowDrawList();
+			timelineDrawlist->AddRectFilled(r_min, r_max, tracklistColor);
+			ImGui::SetCursorScreenPos(cursorTrackListBefore);
+
+			ImVec2 trackCursor = cursorTrackListBefore;
+			for (int i = 0; i < trackCount+2; i++) {
+				ImGui::Text("Track %d", i);
+				ImGui::SameLine(tracklistWidth - 40);
+				ImGui::SmallButton("Mute");
+				trackCursor.y += track1Height;
+				ImGui::SetCursorScreenPos(trackCursor);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+				ImGui::Separator();
+				ImGui::PopStyleVar();
+			}
+			ImGui::Dummy(ImVec2(0, 0)); // workaround.If there is no element(such as text or button or this) after the last track's ImGUI Separator then the SameL
+			ImGui::SetCursorScreenPos(cursorTrackListBefore);
+
+			//ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+			ImGui::EndGroup(); 
+			//ImGui::PopStyleVar();
+		}
+
+		{ // Timeline
+			ImGui::SetCursorScreenPos(cursorTracklistAfter);
+			ImGui::BeginGroup();
+			ImU32 timeline_color = ImGui::GetColorU32(ImVec4(0.25f, 0.25f, 0.25f, 1.0f));
+			bool snappingEnabled = !ImGui::IsKeyDown(ImGuiKey_LeftShift);
+
+			ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+			ImGui::SameLine();
+			ImVec2 childSize = ImVec2(ImGui::GetContentRegionAvail().x, fmax(ImGui::GetContentRegionAvail().y, (trackCount + 3) * track1Height));
+			ImGui::BeginChild("TimelineWindowChild", childSize, false, ImGuiWindowFlags_HorizontalScrollbar);
+
+			bool timelineHovered = ImGui::IsWindowHovered();
+
+			ImGui::SetNextItemAllowOverlap();
+			ImVec2 cursorTimelineBefore = ImGui::GetCursorScreenPos();
+			ImVec2 timeline_size = ImVec2(5000, ImGui::GetContentRegionAvail().y);
+			ImGui::InvisibleButton("timeline", timeline_size);
+			ImGui::PopStyleVar();
+
+			ImVec2 r_min = ImGui::GetItemRectMin();
+			ImVec2 r_max = ImGui::GetItemRectMax();
+			ImVec2 cursorTimelineAfter = ImGui::GetCursorScreenPos();
+			ImGui::GetWindowDrawList()->AddRectFilled(r_min, r_max, timeline_color);
+
+			bool timelineClicked = ImGui::IsItemClicked(ImGuiButtonFlags_MouseButtonLeft);
+
+			ImGui::SetCursorScreenPos(cursorTimelineBefore);
+
+			ImGui::EndChild();
+			ImGui::EndGroup();
+		}
+
+
+	}
+	ImGui::PopStyleVar();
+	ImGui::End();
+
+
 	bool show_demo_window = true;
 	bool show_another_window = true;
 	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
@@ -22,7 +137,8 @@ void UI_DrawEditor(App* app) {
 			ImVec2 displaySize = contentRegion;
 			if (contentRegion.x / contentRegion.y > aspect) {
 				displaySize.x = contentRegion.y * aspect;
-			} else {
+			}
+			else {
 				displaySize.y = contentRegion.x / aspect;
 			}
 
