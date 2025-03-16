ef6c480fb93609a16d3d857da53da0edde0afefc
Jakob <jakobjakobjakob2@gmail.com>
2025-03-16 15:08:43 +0100

    Revert "add cutting support when remuxing video and audio"
    This reverts commit 5c46f212c3461ee8fea1a2cca7ee7156964a2d5d.

--- clipcutter/src/window.cc
+++ clipcutter/src/window.cc
@@ -118,7 +118,6 @@ bool initWindow(App* app) {
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
