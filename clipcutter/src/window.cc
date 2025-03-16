40b10d72ec54cfdd5ac7e0631fce78c6cebad269
Jakob <jakobjakobjakob2@gmail.com>
2025-03-16 15:08:00 +0100

    add cutting support when remuxing video and audio

--- clipcutter/src/window.cc
+++ clipcutter/src/window.cc
@@ -118,6 +118,7 @@ bool initWindow(App* app) {
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
