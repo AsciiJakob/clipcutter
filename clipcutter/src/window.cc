a4f36518efd0c468d89a60a88f1a607e55ae7379
AsciiJakob <jakobjakobjakob2@gmail.com>
2026-03-15 22:34:02 +0100

    fix gamma issues (SRGB color space)

--- clipcutter/src/window.cc
+++ clipcutter/src/window.cc
@@ -14,14 +14,15 @@ bool initWindow(App* app) {
 
     SDL_Window* window = nullptr;
     SDL_GLContext* gl_context = &app->gl_context;
-    const char* glsl_version = "#version 100";
+    const char* glsl_version = "#version 300 es";
 
 
     { // Setup SDL GL
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 0);
 
         // Create window with graphics context
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
